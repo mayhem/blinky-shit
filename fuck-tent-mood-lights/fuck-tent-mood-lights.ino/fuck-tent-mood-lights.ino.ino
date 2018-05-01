89b36deee8b24ac98a6931ad4f600a2a558c6b9b
Robert Kaye <rob@musicbrainz.org>
2018-05-02 00:51:38 +0200

    Fixed up for current hardware

--- fuck-tent-mood-lights/fuck-tent-mood-lights.ino/fuck-tent-mood-lights.ino.ino
+++ fuck-tent-mood-lights/fuck-tent-mood-lights.ino/fuck-tent-mood-lights.ino.ino
@@ -1,7 +1,7 @@
 #include <Adafruit_NeoPixel.h>
 #include <avr/power.h>
 
-#define PIN 3
+#define PIN 4
 
 // Parameter 1 = number of pixels in strip
 // Parameter 2 = Arduino pin number (most are valid)
@@ -10,7 +10,7 @@
 //   NEO_KHZ400  400 KHz (classic 'v1' (not v2) FLORA pixels, WS2811 drivers)
 //   NEO_GRB     Pixels are wired for GRB bitstream (most NeoPixel products)
 //   NEO_RGB     Pixels are wired for RGB bitstream (v1 FLORA pixels, not v2)
-Adafruit_NeoPixel strip = Adafruit_NeoPixel(3, PIN, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(40, PIN, NEO_GRB + NEO_KHZ800);
 
 // IMPORTANT: To reduce NeoPixel burnout risk, add 1000 uF capacitor across
 // pixel power leads, add 300 - 500 Ohm resistor on first pixel's data input
@@ -36,7 +36,7 @@ void loop() {
   //colorWipe(strip.Color(0, 0, 255), 50); // Blue
   // Send a theater pixel chase in...
   //theaterChase(strip.Color(127, 127, 127), 50); // White
-  theaterChase(strip.Color(127,   0,   0), 50); // Red
+  //theaterChase(strip.Color(127,   0,   0), 50); // Red
   //theaterChase(strip.Color(  0,   0, 127), 50); // Blue
 
   rainbow(250);
